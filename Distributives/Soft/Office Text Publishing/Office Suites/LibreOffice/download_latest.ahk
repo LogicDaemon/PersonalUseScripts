e084d6741081386ce5f6d4edb3c33df0ed8e96b3
LogicDaemon <github.com-LogicDaemon@logicdaemon.ru>
2020-12-08 00:45:36 +0300

    add distributive autoupdate script for LibreOffice

---
+++ Distributives/Soft/Office Text Publishing/Office Suites/LibreOffice/download_latest.ahk
@@ -0,0 +1,19 @@
+﻿;by LogicDaemon <www.logicdaemon.ru>
+;This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
+#NoEnv
+FileEncoding UTF-8
+
+EnvGet LocalAppData,LOCALAPPDATA
+EnvGet SystemRoot,SystemRoot
+
+libreOfficeDL := RegexMatch( GetUrl("https://www.libreoffice.org/download/download/")
+                           , "href=""//download\.documentfoundation\.org/libreoffice/stable/(?P<dirVer>[0-9.]+)/win/(?P<dirArch>x[0-9_]+)/LibreOffice_(?P<nameVer>[0-9.]+)_Win_(?P<nameArch>x\d+)\.msi\.torrent"""
+                           , m)
+
+;MsgBox % "dirArch: " mdirArch
+;       . "`ndirVer: " mdirVer
+;       . "`nnameVer: " mnameVer
+;       . "`nnameArch: " mnameArch
+
+RunWait %comspec% /C "%A_ScriptDir%\download_ver.cmd" %mnameVer%,, UseErrorLevel
+Exit %ErrorLevel%
---
+++ Distributives/Soft/Office Text Publishing/Office Suites/LibreOffice/download_ver.cmd
@@ -0,0 +1,33 @@
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
+    SETLOCAL ENABLEEXTENSIONS
+    rem IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
+    rem IF NOT DEFINED baseScripts SET "baseScripts=\Local_Scripts\software_update\Downloader"
+
+    SET "ver=%~1"
+    IF NOT DEFINED ver (ECHO Specify a version!&EXIT /B 1)
+    PUSHD "%~dp032-bit" && (SET "srcpath=%~dp032-bit" & CALL :DownloadArch %1 x86 x86 & POPD)
+    PUSHD "%~dp064-bit" && (SET "srcpath=%~dp064-bit" & CALL :DownloadArch %1 x86_64 x64 & POPD)
+    EXIT /B
+)
+:DownloadArch <ver> <dirArch> <nameArch>
+@(
+    IF NOT DEFINED workdir CALL "%baseScripts%\_GetWorkPaths.cmd"
+    CALL :DownloadTorrent "LibreOffice_%~1_Win_%3.msi.torrent" "https://download.documentfoundation.org/libreoffice/stable/%~1/win/%2/LibreOffice_%~1_Win_%3.msi.torrent"
+    FOR %%L IN ("en-GB" "en-US" "ru") DO @(
+        CALL :DownloadTorrent "LibreOffice_%~1_Win_%3_helppack_%%~L.msi.torrent" "https://download.documentfoundation.org/libreoffice/stable/%~1/win/%2/LibreOffice_%~1_Win_%3_helppack_%%~L.msi.torrent"
+    )
+)
+(
+    SET "TorrentsList="
+    EXIT /B
+)
+:DownloadTorrent <name> <url>
+@(
+    CURL -RL -o %1 %2
+    SET "TorrentsList=%TorrentsList% "
+    rem --file-allocation=falloc requires Admin
+    START "" /B aria2c --file-allocation=trunc --enable-dht6 --seed-time=0 --bt-detach-seed-only --bt-hash-check-seed=false -T %1
+    EXIT /B
+)
