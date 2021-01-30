ae1e7e18e21dcb640a79b1c3ec188b1a2b2053c4
LogicDaemon <github.com-LogicDaemon@logicdaemon.ru>
2021-01-30 12:48:34 +0300

    small changes

--- update.cmd
+++ update.cmd
@@ -6,7 +6,6 @@ robocopy "d:\Distributives" "%~dp0Distributives" *.cmd *.ahk *.list descript.ion
 ahk "%~dp0unpack_Distributives_config.ahk"
 
 robocopy "%USERPROFILE%\Dropbox\Projects\AutoHotkey\Lib" "%~dp0AutoHotkey\Lib" /MIR
-robocopy "%USERPROFILE%\Dropbox\Projects\AutoHotkey\Lib.docs" "%~dp0AutoHotkey\Lib.docs" /MIR
 robocopy "%LocalAppData%\Scripts" "%~dp0misc" /MIR /XD Photo temp-backup-scripts /XF Hotkeys_Custom.ahk KeePass_LogicDaemon.ahk
 robocopy "%LocalAppData%\Scripts\Photo" "%~dp0Photo" /MIR /XD old /XF *.lnk "Copy photos from flash cards.destinations.txt"
 robocopy "%USERPROFILE%\Dropbox\Projects\Setup\Win10" "%~dp0Setup\Win10" /MIR
