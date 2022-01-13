f0c7e4a5b24930ccc4990aa3fb2e5fd589350a1f
LogicDaemon <github.com-LogicDaemon@logicdaemon.ru>
2022-01-13 11:47:19 +0300

    Change destination path for procexp.cmd, procmon.cmd, psexec.cmd

--- misc/psexec.cmd
+++ misc/psexec.cmd
@@ -3,17 +3,23 @@ REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
 SETLOCAL ENABLEEXTENSIONS
 
-    IF NOT EXIST "%LOCALAPPDATA%\Programs\SysUtils\SysInternals" MKDIR "%LOCALAPPDATA%\Programs\SysUtils\SysInternals"
+    SET "suffix="
+    IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET "suffix=64"
+    IF DEFINED PROCESSOR_ARCHITEW6432 SET "suffix=64"
     
-    PUSHD "%LOCALAPPDATA%\Programs\SysUtils\SysInternals" || EXIT /B
-        SET "suffix="
-        IF /I "%PROCESSOR_ARCHITECTURE%"=="AMD64" SET "suffix=64"
-        IF DEFINED PROCESSOR_ARCHITEW6432 SET "suffix=64"
+    SET "destDir=%LOCALAPPDATA%\Programs\SysInternals"
 )
 (
-        CURL -RO -z %~n0%suffix%.exe https://live.sysinternals.com/%~n0%suffix%.exe
-        CURL -RO -z %~n0.chm https://live.sysinternals.com/%~n0.chm
-    POPD
+    IF NOT EXIST "%destDir%" MKDIR "%destDir%"
+    IF EXIST "%destDir%\%~n0%suffix%.exe.bak" DEL "%destDir%\%~n0%suffix%.exe.bak"
+    CURL -RO -z "%destDir%\%~n0.chm" https://live.sysinternals.com/%~n0.chm
+    CURL -R -o "%destDir%\%~n0%suffix%.exe.new" -z "%destDir%\%~n0%suffix%.exe" https://live.sysinternals.com/%~n0%suffix%.exe
+    IF EXIST "%destDir%\%~n0%suffix%.exe.new" (
+        MOVE /Y "%destDir%\%~n0%suffix%.exe.new" "%destDir%\%~n0%suffix%.exe" || (
+            MOVE /Y "%destDir%\%~n0%suffix%.exe" "%destDir%\%~n0%suffix%.exe.bak"
+            MOVE /Y "%destDir%\%~n0%suffix%.exe.new" "%destDir%\%~n0%suffix%.exe"
+        ) || MOVE /Y "%destDir%\%~n0%suffix%.exe.bak" "%destDir%\%~n0%suffix%.exe"
+    )
     ENDLOCAL
-    "%LOCALAPPDATA%\Programs\SysUtils\SysInternals\%~n0%suffix%.exe" %*
+    "%LOCALAPPDATA%\Programs\SysInternals\%~n0%suffix%.exe" %*
 )
