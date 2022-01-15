c9ee6a72e7e5c64cdccafa0939a446292ee982d8
LogicDaemon <github.com-LogicDaemon@logicdaemon.ru>
2022-01-15 13:10:00 +0300

    Fix Vivaldi distributives downloading, implement download_appcast.py for that

--- Distributives/Soft com freeware/Network/HTTP/Vivaldi/.Distributives_Update_Run.All.cmd
+++ Distributives/Soft com freeware/Network/HTTP/Vivaldi/.Distributives_Update_Run.All.cmd
@@ -5,7 +5,9 @@ SETLOCAL ENABLEEXTENSIONS
 )
 (
 rem     https://downloads.vivaldi.com/stable/Vivaldi.2.11.1811.52.x64.exe
-    CALL "%baseScripts%\_DistDownload.cmd" https://vivaldi.com *.x64.exe -ml1 -A.exe -nd "-e robots=off" --user-agent="Mozilla/5.0 (Windows NT 5.4; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0" -HD downloads.vivaldi.com,vivaldi.com
-    SET "srcpath=%srcpath%32-bit\"
-    CALL "%baseScripts%\_DistDownload.cmd" https://vivaldi.com *.exe -ml1 -A.exe -nd "-e robots=off" -HD downloads.vivaldi.com,vivaldi.com
+rem     CALL "%baseScripts%\_DistDownload.cmd" https://update.vivaldi.com/update/1.0/public/appcast.x64.xml *.x64.exe -ml1 -A.exe -nd "-e robots=off" --user-agent="Mozilla/5.0 (Windows NT 5.4; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0" -HD downloads.vivaldi.com,vivaldi.com
+rem     SET "srcpath=%srcpath%32-bit\"
+rem     CALL "%baseScripts%\_DistDownload.cmd" https://update.vivaldi.com/update/1.0/public/appcast.xml *.exe -ml1 -A.exe -nd "-e robots=off" -HD downloads.vivaldi.com,vivaldi.com
+    py "%baseScripts%\download_appcast.py" https://update.vivaldi.com/update/1.0/public/appcast.x64.xml
+    py "%baseScripts%\download_appcast.py" https://update.vivaldi.com/update/1.0/public/appcast.xml
 )
