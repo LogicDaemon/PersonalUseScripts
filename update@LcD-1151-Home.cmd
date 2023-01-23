92938f20766bd7b73aa9da47c95b878ed42c9d84
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-01-23 08:33:48 +0300

    Allow splitting update-script per host

---
+++ update@LcD-1151-Home.cmd
@@ -0,0 +1,16 @@
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
+SETLOCAL ENABLEEXTENSIONS
+robocopy "d:\Distributives\config" "%~dp0Distributives\config" /MIR /S /XD d:\Distributives\config\_Scripts\Lib
+robocopy "d:\Distributives" "%~dp0Distributives" *.cmd *.ahk *.list descript.ion "partial list of free SysUtils.txt" jre_install_common.cfg opabackup342.exe.config /MIR /S /XD config Drivers_local Local_Scripts wsusoffline
+ahk "%~dp0unpack_Distributives_config.ahk"
+
+robocopy "%USERPROFILE%\Dropbox\Projects\AutoHotkey\Lib" "%~dp0AutoHotkey\Lib" /MIR
+robocopy "%LocalAppData%\Scripts" "%~dp0misc" /MIR /XD Photo temp-backup-scripts /XF Hotkeys_Custom.ahk KeePass_LogicDaemon.ahk
+robocopy "%LocalAppData%\Scripts\Photo" "%~dp0Photo" /MIR /XD old /XF *.lnk "Copy photos from flash cards.destinations.txt"
+robocopy "%USERPROFILE%\Dropbox\Projects\Setup\Win10" "%~dp0Setup\Win10" /MIR
+robocopy "%USERPROFILE%\Dropbox\Projects\Setup" "%~dp0Setup" "Logitech Gaming Software en-US.reg"
+ATTRIB -R -S -H /S "%~dp0"
+ATTRIB +H "%~dp0.git"
+)
