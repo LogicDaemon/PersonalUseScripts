0a12ade2456494abda648a89b0f222270ed2de3c
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-02-07 15:33:40 +0300

    Add git_initial_config.cmd

---
+++ misc/git_initial_config.cmd
@@ -0,0 +1,5 @@
+git config --system core.autocrlf false
+git config --system core.longpaths true
+git config --system core.symlinks true
+git config --system core.filemode false
+git config --global diff.algorithm histogram
