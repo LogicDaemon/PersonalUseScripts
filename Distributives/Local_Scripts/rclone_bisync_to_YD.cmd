abe2c4e62d58dc7cf6f15bdf5f5421d1c6eb7bb5
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-02-16 08:37:54 +0300

    [Local_Scripts] Add backup (rclone) scripts and run_updates.cmd

---
+++ Distributives/Local_Scripts/rclone_bisync_to_YD.cmd
@@ -0,0 +1,39 @@
+(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
+SETLOCAL ENABLEEXTENSIONS
+    CHCP 65001
+    IF EXIST rclone_to_YD.log CALL :RenameLog rclone_to_YD.log -remains
+    ECHO. >rclone_to_YD.log
+    COMPACT /C rclone_to_YD.log
+    SET "errorRename="
+)
+(CALL bisync_with_YD.cmd "V:\Distributives" --filters-file "%~dp0rclone-filters.txt" %* 2>&1 || SET "errorRename=1") | tee -a "rclone_to_YD.log"
+(
+    COMPACT /C /F /EXE:LZX rclone_to_YD.log
+    IF DEFINED errorRename (
+        REN "rclone_to_YD.log" "%~dp0rclone_to_YD.error%DATE:/=_%_%TIME::=_%.log"
+    ) ELSE (
+        MOVE /Y rclone_to_YD.log rclone_to_YD.last_ok.log
+    )
+    EXIT /B
+)
+
+:RenameLog <path> <suffix>
+@(
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET "timesuffix=%~t1"
+SET "timesuffix=!timesuffix::=!"
+SET "timesuffix=!timesuffix:/=!"
+SET "timesuffix=!timesuffix:\=!"
+)
+@(
+ENDLOCAL
+SETLOCAL
+SET newname="%%~n1%~2%timesuffix%%%~x1"
+)
+(
+REN %%A "%newname%"
+@START "" /B COMPACT /C /F /EXE:LZX "%~dp1%newname%"
+@EXIT /B
+)
