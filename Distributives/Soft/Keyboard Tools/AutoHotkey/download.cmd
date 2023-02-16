a847dc9ab6d444b74f8afcb047065a1f68be997a
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-02-16 08:36:41 +0300

    [AutoHotkey] Add download/install scripts

--- Distributives/Soft/Keyboard Tools/AutoHotkey/download.cmd
+++ Distributives/Soft/Keyboard Tools/AutoHotkey/download.cmd
@@ -2,19 +2,8 @@
     SETLOCAL ENABLEEXTENSIONS
     IF "%~dp0"=="" (SET "srcpath=%CD%\") ELSE SET "srcpath=%~dp0"
     IF NOT DEFINED baseScripts SET "baseScripts=\Local_Scripts\software_update\Downloader"
-
-    SET "url=https://autohotkey.com/download/ahk-install.exe"
-    SET "urlfname=ahk-install.exe"
-    SET "distfmask=AutoHotkey_*_setup.exe"
 )
-FOR /F "usebackq delims=" %%A IN (`DIR /B /O-D "%srcpath%%distfmask%"`) DO (
-    SET "curDistPath=%srcpath%%%~A"
-    SET "curDistName=%%~nxA"
-    SET timeCond=-z "%srcpath%%%~A"
-    GOTO :ExitcurDistPathFor
-)
-:ExitcurDistPathFor
-(
+@(
     CALL "%baseScripts%\_GetWorkPaths.cmd"
     rem srcpath with baseDistUpdateScripts replaced to baseDistributives
     rem relpath is srcpath relatively to baseDistributives (no trailing backslash)
@@ -22,78 +11,82 @@ FOR /F "usebackq delims=" %%A IN (`DIR /B /O-D "%srcpath%%distfmask%"`) DO (
     rem logsDir - baseLogsDir with relpath (or nothing)
     IF NOT DEFINED workdir SET "workdir=%srcpath%temp\"
 )
-(
+@(
     IF NOT DEFINED logsDir SET "logsDir=%workdir%"
-    IF EXIST "%workdir%new.tmp" RD /S /Q "%workdir%new.tmp"
+    
+    CALL :DownloadDistributive https://autohotkey.com/download/ahk.zip "AutoHotkey_*.zip"
+    CALL :DownloadDistributive https://autohotkey.com/download/ahk-install.exe "AutoHotkey_*_setup.exe"
+    RD /Q "%workdir%"
+    EXIT /B
+)
+:DownloadDistributive
+(
+    SETLOCAL
+    SET "url=%~1"
+    SET "urlfname=%~nx1"
+    SET "distfmask=%~2"
+)
+IF NOT DEFINED distfmask SET "distfmask=%urlfname%"
+CALL :GetExt expected_ext "%distfmask%"
+FOR /F "usebackq delims=" %%A IN (`DIR /B /O-D "%srcpath%%distfmask%"`) DO (
+    SET "curDistPath=%srcpath%%%~A"
+    SET "curDistName=%%~nxA"
+    REM on 2022 and years before, any -z condition causes 0-sized file to be downloaded
+    SET timeCond=-z "%srcpath%%%~A"
+    GOTO :ExitFor_CurDistPath
+)
+:ExitFor_CurDistPath
+(
     MKDIR "%workdir%new.tmp"
-    rem xln.exe "%curDistPath%" "%workdir%new.tmp\%curDistName%"
-    rem START "" /B /WAIT /D "%workdir%new.tmp" wget.exe -N %url%
-    START "" /B /WAIT /D "%workdir%new.tmp" curl "%url%" %timeCond% -OJR -H "authority: autohotkey.com" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3" -H "accept-encoding: gzip, deflate, br" -H "accept-language: ru,en-GB;q=0.9,en;q=0.8,en-US;q=0.7" || CALL :ExitWithError running CURL
-    
-    IF NOT EXIST "%workdir%new.tmp\*.*" (
-	rem CURL still ignores server filename. Have no idea what to do with it. So it'll be only used as backup.
-	rem -J, --remote-header-name  Use the header-provided filename (H)
-	rem -k, --insecure      Allow connections to SSL sites without certs (H)
-	rem -L, --location      Follow redirects (H)
-	rem -o, --output FILE   Write to FILE instead of stdout
-	rem -#, --progress-bar  Display transfer progress as a progress bar
-	rem -p, --proxytunnel   Operate through a HTTP proxy tunnel (using CONNECT)
-	rem -O, --remote-name   Write output to a file named as the remote file
-	rem     --remote-name-all  Use the remote file name for all URLs
-	rem -R, --remote-time   Set the remote file's time on the local output
-	rem -z, --time-cond TIME   Transfer based on a time condition
-
-	START "" /B /WAIT /D "%workdir%new.tmp" c:\SysUtils\curl.exe "%url%" -LpR --remote-name-all %timeCond% || (CALL :ExitWithError running CURL & EXIT /B 1)
-    )
-    
-    rem without -o for CURL and -O for wget, filename is unknown
-    FOR %%A IN ("%workdir%new.tmp\*.exe") DO (	
-        ECHO Checking %%~nxA
-        IF "%%~zA"=="0" (
-            ECHO Zero size, removing.
-            DEL "%%~A"
-        ) ELSE (
-            SET "dlfname=%%~nxA"
-            CALL :GetFileVer ver "%%~A" || SET "ver="
+    FOR %%A IN ("-LORJ" "-LOR") DO (
+        START "" /B /WAIT /D "%workdir%new.tmp" curl "%url%" %timeCond% %%~A -H "authority: autohotkey.com" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-GB,en-US;q=0.9,en;q=0.8"
+        REM on 2022 and years before, any -z condition causes 0-sized file to be downloaded
+        FOR %%B IN ("%workdir%new.tmp\*.*") DO @IF "%%~zB"=="0" DEL "%%~B"
+        rem without -o for CURL and -O for wget, filename is unknown
+        FOR %%D IN ("%workdir%new.tmp\*%expected_ext%") DO (	
+            ECHO Checking %%~nxD
+            SET "dlfname=%%~nxD"
+            SET "ver="
+            CALL :GetFileVer ver "%%~D" || CALL :GetVerFromName ver "%%~D"
             
-            IF "%%~nxA"=="%urlfname%" (
-                IF NOT DEFINED ver (
-                    ECHO Could not determine version of "%%~nxA", and it's used to define filename
+            IF "%%~nxD"=="%urlfname%" (
+                IF DEFINED ver (
+                    CALL :ReplaceStarWithStr dstfname "%distfmask%" "%ver%"
                 ) ELSE (
-                    FOR /F "usebackq tokens=2 delims=_" %%B IN ("%%~nA") DO SET "dstfname=AutoHotkey_%%~B_setup.exe"
+                    ECHO Could not determine version of "%%~nxD", and it's used to define filename
                 )
             )
-            IF NOT DEFINED dstfname SET "dstfname=%%~nxA"
+            IF NOT DEFINED dstfname SET "dstfname=%%~nxD"
+            CALL :PrintDstFname
             IF DEFINED ver GOTO :ExitGetVerLoop
         )
     )
+    
     IF NOT DEFINED dlfname CALL :ExitWithError Nothing downloaded & EXIT /B 1
 )
 :ExitGetVerLoop
 (
-    IF NOT "%ver%"=="" ( REM when destination is on a remote, somehow ver is defined but empty string
+    IF NOT "%ver%"=="" ( REM when destination is on a remote, somehow ver is defined but is empty string
         IF NOT "%ver:~0,2%"=="1." (
 	    ( CALL :ExitWithError "Version %ver% downloaded from %url% (must be version 1.*), aborting"
 	    EXIT /B 1
 	    ) > "%srcpath%warning.txt"
         )
+        ( ECHO %ver%	%dstfname%
+        ) > "%srcpath%newver%expected_ext%.txt"
+        CALL :movedst "%workdir%new.tmp\%dlfname%" && MOVE /Y "%srcpath%newver%expected_ext%.txt" "%srcpath%ver%expected_ext%.txt"
     ) ELSE IF "%dlfname%"=="%dlfname:_1.=%" IF "%dlfname%"=="%dlfname: 1.=%" (
         CALL :ExitWithError "Unknown version (file name %dlfname%) downloaded from %url%, aborting"
 	EXIT /B 1
-    ) > "%srcpath%warning.txt"
+    ) >> "%srcpath%warning.txt"
     
-    ( ECHO %ver%	%dstfname%
-    ) > "%srcpath%newver.txt"
-    rem SET "dstfname=AutoHotkey_%ver%_setup.exe"
-    CALL :movedst "%workdir%new.tmp\%dlfname%" && MOVE /Y "%srcpath%newver.txt" "%srcpath%ver.txt"
-    RD "%workdir%new.tmp"
+    RD /Q "%workdir%new.tmp"
 EXIT /B 0
 )
 :movedst
 (
     SET cleanup_action=CALL "%baseScripts%\mvold.cmd"
     CALL "%baseScripts%\DistCleanup.cmd" "%srcpath%%distfmask%" "%srcpath%%dstfname%"
-rem     %SystemDrive%\SysUtils\xln.exe %1 "%srcpath%%dstfname%"||
     MOVE /Y %1 "%srcpath%%dstfname%"
 EXIT /B
 )
@@ -104,14 +97,54 @@ EXIT /B
 EXIT /B
 )
 :GetFileVer <varname> <path>
+@(
+    SETLOCAL
+    SET "fileNameForWMIC=%~2"
+)
+@SET "fileNameForWMIC=%fileNameForWMIC:\=\\%"
 (
-SETLOCAL
-SET "fileNameForWMIC=%~2"
+    FOR /F "usebackq skip=1" %%I IN (`wmic datafile where Name^="%fileNameForWMIC%" get Version`) DO @(
+        REM IF NOT "%%~I"=="" does not work here, it's always unequal, but it looks like there's \n or something nasty is in the loop var
+        REM it works after assigning to the env var though
+        SET "verWMIC=%%~I"
+        CALL :GetFileVerCheckEmpty tmp_ver && GOTO :GetFileVer_done
+    )
 )
-SET fileNameForWMIC=%fileNameForWMIC:\=\\%
-FOR /F "usebackq skip=1" %%I IN (`wmic datafile where Name^="%fileNameForWMIC%" get Version`) DO (
+:GetFileVer_done
+@(
     ENDLOCAL
-    SET "%~1=%%~I"
-    EXIT /B 0
+    IF "%tmp_ver%"=="" EXIT /B 1
+    SET "%~1=%tmp_ver%"
+    EXIT /B
 )
-EXIT /B 1
+:GetVerFromName <varname> <path>
+(
+    SETLOCAL
+    REM name example: AutoHotkey_1.1.34.03.zip
+    FOR /F "delims=_ tokens=2" %%I IN ("%~n2") DO @IF NOT "%%~I"=="" (
+        ENDLOCAL
+        SET "%~1=%%~I"
+        EXIT /B 0
+    )
+    EXIT /B 1
+)
+:GetFileVerCheckEmpty
+@(
+    ENDLOCAL
+    IF NOT "%verWMIC%"=="" (
+        SET "%~1=%verWMIC%"
+        EXIT /B 0
+    )
+    EXIT /B 1
+)
+
+:GetExt <var> <path>
+@(
+    SET "%~1=%~x2"
+    EXIT /B
+)
+:ReplaceStarWithStr <varname> <str_with_a_star> <replacement>
+@(
+    FOR /F "tokens=1* delims=*" %%A IN (%2) DO SET "%~1=%%~A%~3%%~B"
+    EXIT /B
+)
