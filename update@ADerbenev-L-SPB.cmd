00e258734a4e5cf9442f3c317c6e71b38b231ac2
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-05-29 09:46:28 +0300

    misc hiccups

--- update@ADerbenev-L-SPB.cmd
+++ update@ADerbenev-L-SPB.cmd
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEEXTENSIONS
 ln --unroll --recursive --mirror "%USERPROFILE%\Documents\AutoHotkey\Lib" "%~dp0AutoHotkey\Lib"
 @REM TODO: cipher /U /N /H ., and parse the output to skip encrypted files instead of hardcoding them here
-ln --excludedir .mypy_cache --excludedir connect-asg-host --excludedir helper_shortcuts --excludedir Photo --excludedir temp-backup-scripts --excludedir software_update --exclude Hotkeys_Custom.ahk --exclude Hotkeys_Custom.*.ahk --exclude KeePass_*.ahk --exclude *.lnk --exclude _Distributives.base_dirs.txt --exclude aws_s.cmd --exclude aws_t.cmd --unroll --recursive --mirror  "%LOCALAPPDATA%\Scripts" "%~dp0misc"
+ln --excludedir .mypy_cache --excludedir connect-asg-host --excludedir helper_shortcuts --excludedir Photo --excludedir temp-backup-scripts --excludedir software_update --exclude Hotkeys_Custom.ahk --exclude Hotkeys_Custom.*.ahk --exclude *.lnk --exclude _Distributives.base_dirs.txt --exclude aws_s.cmd --exclude aws_t.cmd --unroll --recursive --mirror  "%LOCALAPPDATA%\Scripts" "%~dp0misc"
 ln --unroll --recursive --mirror "%LOCALAPPDATA%\Scripts\.vscode" "%~dp0misc\.vscode"
 ln --excludedir old --exclude *.lnk --exclude "Copy photos from flash cards.destinations.txt" --unroll --recursive --mirror  "%LOCALAPPDATA%\Scripts\Photo" "%~dp0Photo"
 CALL "%~dp0update.finalize.cmd"
