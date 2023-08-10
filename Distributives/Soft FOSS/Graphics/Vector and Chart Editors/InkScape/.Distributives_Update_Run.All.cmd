6a74779fc2e17de4c1829b3c0e80268d31caa849
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-08-10 09:26:27 +0300

    misc

---
+++ Distributives/Soft FOSS/Graphics/Vector and Chart Editors/InkScape/.Distributives_Update_Run.All.cmd
@@ -0,0 +1,8 @@
+@(REM coding:CP866
+REM by LogicDaemon <www.logicdaemon.ru>
+REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode>.
+SETLOCAL ENABLEEXTENSIONS
+
+SET "srcpath=%~dp0"
+CALL py.cmd -O "%~dp0download.py" -t "windows/64-bit/compressed-7z"
+)
