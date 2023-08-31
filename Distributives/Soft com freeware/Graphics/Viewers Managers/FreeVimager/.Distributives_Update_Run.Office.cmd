c44cac9ebfdc266c65c6abc0b31843032a16d4ac
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-09-01 01:16:45 +0400

    Fix urls in FreeVImager downloading script

--- Distributives/Soft com freeware/Graphics/Viewers Managers/FreeVimager/.Distributives_Update_Run.Office.cmd
+++ Distributives/Soft com freeware/Graphics/Viewers Managers/FreeVimager/.Distributives_Update_Run.Office.cmd
@@ -7,8 +7,8 @@
     SET "distcleanup=1"
     SET "AddtoS_UScripts=0"
     rem FreeVImager is Donationware since ver 9.0.
-    CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/russian/ FreeVimager-*-Portable-Rus.exe -ml1 -nd -A.exe
-    CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/russian/ FreeVimager-*-Setup-Rus.exe -ml1 -nd -A.exe
-    CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/english/ FreeVimager-*-Portable.exe -ml1 -nd -A.exe
-    CALL "%baseScripts%\_DistDownload.cmd" http://www.contaware.com/downloads/latest/english/ FreeVimager-*-Setup.exe -ml1 -nd -A.exe
+    CALL "%baseScripts%\_DistDownload.cmd" https://www.contaware.com/downloads/latest/FreeVimager/russian/ FreeVimager-*-Portable-Rus.exe -ml1 -nd -A.exe
+    CALL "%baseScripts%\_DistDownload.cmd" https://www.contaware.com/downloads/latest/FreeVimager/russian/ FreeVimager-*-Setup-Rus.exe -ml1 -nd -A.exe
+    CALL "%baseScripts%\_DistDownload.cmd" https://www.contaware.com/downloads/latest/FreeVimager/english/ FreeVimager-*-Portable.exe -ml1 -nd -A.exe
+    CALL "%baseScripts%\_DistDownload.cmd" https://www.contaware.com/downloads/latest/FreeVimager/english/ FreeVimager-*-Setup.exe -ml1 -nd -A.exe
 )
