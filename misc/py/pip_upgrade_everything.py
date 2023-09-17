739474c1c8d0648c28e90966634fb74e0b255d72
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-09-17 22:42:04 +0400

    misc

--- misc/py/pip_upgrade_everything.py
+++ misc/py/pip_upgrade_everything.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import sys
 import pkg_resources
 from subprocess import call
 
 packages = [dist.project_name for dist in pkg_resources.working_set]  # pylint: disable=not-an-iterable
 print(packages)
-call('pip install --upgrade --upgrade-strategy eager ' + ' '.join(packages), shell=True)
+call(f'"{sys.executable}" -m pip install --upgrade --upgrade-strategy eager ' + ' '.join(packages), shell=True)
