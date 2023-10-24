1f0c65247a03ab1f89333dc76d82ba3def4a02e2
LogicDaemon <LogicDaemon@users.noreply.github.com>
2023-10-24 09:06:14 +0300

    Add filtering to avoid empty GUI change lists in GUI Unison

--- misc/sync_ADerbenev-L-SPB.cmd
+++ misc/sync_ADerbenev-L-SPB.cmd
@@ -1,4 +1,5 @@
 @(REM coding:CP866
+NET USE \\AcerPH315-53-71HN
 SET "hostname=ADerbenev-L-SPB"
 CALL "%~dp0sync_with_unison_server.cmd" %*
 )
--- misc/sync_ADerbenev-L-SPB_with_unison_server.cmd
+++ misc/sync_ADerbenev-L-SPB_with_unison_server.cmd
@@ -3,7 +3,22 @@ REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
 SETLOCAL ENABLEEXTENSIONS
     IF NOT DEFINED syncprog CALL _unison_get_command.cmd
+    SET "filterSyncs="
+    IF "%unisonopt%"=="" SET "filterSyncs=1"
+    IF "%unisonopt%"=="-auto" SET "filterSyncs=1"
 )
 @IF NOT DEFINED syncprog SET "syncprog=%unisontext%"
-%unisontext% Distributives_AcerPH315-53-71HN -path "Soft/Keyboard Tools/AutoHotkey/ver.zip.txt" -prefer "\\AcerPH315-53-71HN\Distributives$" -auto -batch
-@FOR %%A IN ("%USERPROFILE%\.unison\*_AcerPH315-53-71HN.prf") DO %syncprog% "%%~nA" %unisonopt%
+@(
+    IF DEFINED filterSyncs (
+        %unisontext% Distributives_AcerPH315-53-71HN -path "Soft/Keyboard Tools/AutoHotkey/ver.zip.txt" -prefer "\\AcerPH315-53-71HN\Distributives$" -auto -batch
+        DEL "%TEMP%\%~n0.list.txt"
+        FOR %%A IN ("%USERPROFILE%\.unison\*_AcerPH315-53-71HN.prf") DO @(
+            <NUL %unisontext% "%%~nA" "-auto=false" || ECHO "%%~nA">>"%TEMP%\%~n0.list.txt"
+        )
+        FOR /F "usebackq delims=" %%A IN ("%TEMP%\%~n0.list.txt") DO %syncprog% "%%~A" %unisonopt%
+        DEL "%TEMP%\%~n0.list.txt"
+    ) ELSE (
+        FOR %%A IN ("%USERPROFILE%\.unison\*_AcerPH315-53-71HN.prf") DO %syncprog% "%%~nA" %unisonopt%
+    )
+EXIT /B
+)
