110f6fafc529b512d489c2d192aaa0b6c01eb91d
LogicDaemon <LogicDaemon@users.noreply.github.com>
2024-01-21 22:47:09 +0400

    misc

---
+++ Distributives/Soft FOSS/Office Text Publishing/Text Documents/Visual Studio Code Insiders/download_VSCode-win32-x64-1.86.0-insider-271fb7fbd599b49a1482ea7284a50b3229317f96.zip.cmd
@@ -0,0 +1,4 @@
+@(REM coding:CP866
+PUSHD "" || EXIT /B
+C:\WINDOWS\System32\curl.exe -R -z "D:\Distributives\Soft FOSS\Office Text Publishing\Text Documents\Visual Studio Code Insiders\VSCode-win32-x64-1.86.0-insider-271fb7fbd599b49a1482ea7284a50b3229317f96.zip" -o "D:\Distributives\Soft FOSS\Office Text Publishing\Text Documents\Visual Studio Code Insiders\VSCode-win32-x64-1.86.0-insider-271fb7fbd599b49a1482ea7284a50b3229317f96.zip.tmp" -- "https://vscode.download.prss.microsoft.com/dbazure/download/insider/271fb7fbd599b49a1482ea7284a50b3229317f96/VSCode-win32-x64-1.86.0-insider.zip"
+)
