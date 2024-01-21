1abbace848e0777e5cb6d276431bf8fa5c17a01f
LogicDaemon <LogicDaemon@users.noreply.github.com>
2024-01-21 22:43:49 +0400

    misc

--- misc/compact_lzx_ProgramsDirs.cmd
+++ misc/compact_lzx_ProgramsDirs.cmd
@@ -3,11 +3,13 @@ REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
 SETLOCAL ENABLEEXTENSIONS
 rem     SET "e=*.exe *.dll *.mui"
-    SET "e=*.pdb *.sys *.eml *.qml *.js *.pyd *.py *.pyi *.qmltypes *.rcc *.inf *.manifest"
+    SET "extra=*.pdb *.sys *.eml *.qml *.js *.pyd *.py *.pyi *.qmltypes *.rcc *.inf *.manifest"
+    SET "exe=*.dll *.exe"
 )
 (
-    FOR /R "%ProgramFiles(x86)%" %B IN (%e%) DO @IF %~zB GTR 4095 START "%~B" /low compact /C /Q /EXE:LZX "%~B"
-    FOR /R "%ProgramFiles%" %B IN (%e%) DO @IF %~zB GTR 4095 START "%~B" /low compact /C /Q /EXE:LZX "%~B"
-    FOR /R "%USERPROFILE%" %B IN (%e%) DO @IF %~zB GTR 4095 START "%~B" /low compact /C /Q /EXE:LZX "%~B"
-    FOR /R "w:\Temp" %B IN (%e%) DO @IF %~zB GTR 4095 START "%~B" /low compact /C /Q /EXE:LZX "%~B")
+    COMPACT /C /Q /EXE:LZX /S:"v:\Program Files (x86)" %extra% %exe%
+rem     FOR /R "%ProgramFiles(x86)%" %%B IN (%extra%) DO @IF %%~zB GEQ 4096 START "%%~B" /low compact /C /Q /EXE:LZX "%%~B"
+rem     FOR /R "%ProgramFiles%" %%B IN (%extra%) DO @IF %%~zB GEQ 4096 START "%%~B" /low compact /C /Q /EXE:LZX "%%~B"
+rem     FOR /R "%USERPROFILE%" %%B IN (%extra% %exe%) DO @IF %%~zB GEQ 4096 START "%%~B" /low compact /C /Q /EXE:LZX "%%~B"
+rem     FOR /R "w:\Temp" %%B IN (%extra% %exe%) DO @IF %%~zB GEQ 4096 START "%%~B" /low compact /C /Q /EXE:LZX "%%~B")
 )
