942ee1fee2cf4bdf51bb86485234077789a2fbb7
LogicDaemon <LogicDaemon@users.noreply.github.com>
2024-01-21 22:46:10 +0400

    Fix DesktopOk download script

--- Distributives/Soft com freeware/System/Other/DesktopOK/.Distributives_Update_Run.Home.cmd
+++ Distributives/Soft com freeware/System/Other/DesktopOK/.Distributives_Update_Run.Home.cmd
@@ -6,16 +6,16 @@ SETLOCAL ENABLEEXTENSIONS
     IF NOT DEFINED baseScripts SET baseScripts=\Scripts
     CALL "%baseScripts%\_GetWorkPaths.cmd"
 )
-@IF NOT DEFINED workdir SET "workdir=%srcpath%\temp\"
-@(
+IF NOT DEFINED workdir SET "workdir=%srcpath%\temp\"
+(
     MKDIR "%workdir%" 2>NUL
-    CALL :download http://www.softwareok.com/Download/DesktopOK_Unicode.zip
-    CALL :download http://www.softwareok.com/Download/DesktopOK_x64.zip
+    CALL :download "http://www.softwareok.com/Download/DesktopOK_Unicode.zip"
+    CALL :download "http://www.softwareok.com/Download/DesktopOK_x64.zip"
 EXIT /B
 )
 :download <url>
-@(
-    IF EXIT "%workdir%%~nx1.tmp" DEL "%workdir%%~nx1.tmp" || EXIT /B
+(
+    IF EXIST "%workdir%%~nx1.tmp" (DEL "%workdir%%~nx1.tmp" || EXIT /B)
     CURL -RJL -o "%workdir%%~nx1.tmp" -z "%srcpath%%~nx1" %1 || EXIT /B
     IF NOT EXIST "%workdir%%~nx1.tmp" EXIT /B
     ECHO Moving "%workdir%%~nx1.tmp" to "%srcpath%%~nx1"
