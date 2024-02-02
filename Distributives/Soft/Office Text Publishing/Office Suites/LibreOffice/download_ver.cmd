986f82cb341e0e3d94729b0b71d8c9f2d998b72f
LogicDaemon <LogicDaemon@users.noreply.github.com>
2024-02-02 22:11:42 +0400

    Fix version comparision in LibreOffice downloading script

--- Distributives/Soft/Office Text Publishing/Office Suites/LibreOffice/download_ver.cmd
+++ Distributives/Soft/Office Text Publishing/Office Suites/LibreOffice/download_ver.cmd
@@ -10,11 +10,12 @@ REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 In
     PUSHD "%~dp032-bit" && (SET "srcpath=%~dp032-bit" & CALL :DownloadArch %1 x86 x86 & POPD)
     PUSHD "%~dp064-bit" && (
         SET "srcpath=%~dp064-bit" & (
-            IF "%~1" LEQ "7.5." (
+            CALL :VerAtLeast "%~1" "7.5"
+            IF ERRORLEVEL 1 (
+                CALL :DownloadArch %1 x86_64 x86-64
+            ) ELSE (
                 rem This changed with release 7.5.1, but still works for older releases
                 CALL :DownloadArch %1 x86_64 x64 & POPD
-            ) ELSE (
-                CALL :DownloadArch %1 x86_64 x86-64
             )
         )
         POPD
@@ -42,4 +43,35 @@ REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 In
     rem --file-allocation=falloc requires Admin
     START "" /B aria2c --file-allocation=trunc --enable-dht6 --seed-time=0 --bt-detach-seed-only --bt-hash-check-seed=false --check-integrity=true -T %1
     EXIT /B
+)
+:VerAtLeast <ver_to_compare> <required_ver>
+(
+SETLOCAL
+FOR /F "delims=. tokens=1,2,3,4" %%I IN ("%~1") DO (
+    SET ver1sub1=%%I
+    SET ver1sub2=%%J
+    SET ver1sub3=%%K
+    SET ver1sub4=%%L
+)
+FOR /F "delims=. tokens=1,2,3,4" %%I IN ("%~2") DO (
+    SET ver2sub1=%%I
+    SET ver2sub2=%%J
+    SET ver2sub3=%%K
+    SET ver2sub4=%%L
+)
+IF NOT DEFINED ver1sub2 SET ver1sub2=0
+IF NOT DEFINED ver1sub3 SET ver1sub3=0
+IF NOT DEFINED ver1sub4 SET ver1sub4=0
+IF NOT DEFINED ver2sub2 SET ver2sub2=0
+IF NOT DEFINED ver2sub3 SET ver2sub3=0
+IF NOT DEFINED ver2sub4 SET ver2sub4=0
+)
+(
+ENDLOCAL
+rem No quotes to compare as numbers
+IF %ver2sub1% GTR %ver1sub1% EXIT /B 1
+IF %ver2sub2% GTR %ver1sub2% EXIT /B 1
+IF %ver2sub3% GTR %ver1sub3% EXIT /B 1
+IF %ver2sub4% GTR %ver1sub4% EXIT /B 1
+EXIT /B 0
 )
