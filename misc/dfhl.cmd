a953c126bbc0386525ca807ae4da6e256c256f7e
LogicDaemon <LogicDaemon@users.noreply.github.com>
2024-06-09 10:03:19 +0400

    misc

--- misc/dfhl.cmd
+++ misc/dfhl.cmd
@@ -1,2 +1,7 @@
-@REM coding:CP866
-"%LocalAppData%\Programs\DFHL_2.6\DFHL.exe" /l /r /w %* 2>&1 | tee -a DFHL.log
+@(REM coding:CP866
+IF /I "%CD%"=="%WINDIR%" (
+    ECHO This batch cannot work if started with a network drive as working directory. Use dfhl.exe instead.
+    EXIT /B 1
+)
+"%LocalAppData%\Programs\DFHL_2.6\DFHL.exe" /w %* 2>&1 | tee -a DFHL.log
+)
