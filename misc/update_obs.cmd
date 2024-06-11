00caefb17c49e58e189ee1889bb1152f869b2646
LogicDaemon <LogicDaemon@users.noreply.github.com>
2024-06-11 08:28:34 +0300

    Fix Distributives subpaths in update_obs.cmd

--- misc/update_obs.cmd
+++ misc/update_obs.cmd
@@ -3,9 +3,13 @@ REM by LogicDaemon <www.logicdaemon.ru>
 REM This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License <https://creativecommons.org/licenses/by-sa/4.0/legalcode.ru>.
 SETLOCAL ENABLEEXTENSIONS
 
-CALL "%~dp0_Distributives.find_subpath.cmd" Distributives "Soft FOSS\MultiMedia\Capture\OBS\download.cmd"
+    CALL "%~dp0_Distributives.find_subpath.cmd" Distributives "Soft FOSS\MultiMedia\Capture\OBS\install_to_LOCALAPPDATA.cmd"
+    IF NOT DEFINED Distributives (
+        ECHO Could not find Distributives directory with OBS
+        EXIT /B 1
+    )
 )
-@(
-CALL "%Distributives%\Soft FOSS\MultiMedia\Capture\OBS\download.cmd"
-CALL "%Distributives%\Soft FOSS\MultiMedia\Capture\OBS\install_to_LOCALAPPDATA.cmd"
+(
+    FOR %%A IN ("%Distributives%\Soft FOSS\MultiMedia\Capture\OBS\.Distributives_Update_Run.*.cmd") DO CALL "%%~A"
+    CALL "%Distributives%\Soft FOSS\MultiMedia\Capture\OBS\install_to_LOCALAPPDATA.cmd"
 )
