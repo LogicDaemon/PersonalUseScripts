c8c7f72fdf2fb223e02a44c202e02d5a35afc99e
LogicDaemon <LogicDaemon@users.noreply.github.com>
2025-03-13 09:00:09 +0400

    Use regular user permissions for Vivaldi LOCALAPPDATA install even if run as admin

--- Distributives/Soft com freeware/Network/HTTP/Vivaldi/install_to_LOCALAPPDATA_Programs.cmd
+++ Distributives/Soft com freeware/Network/HTTP/Vivaldi/install_to_LOCALAPPDATA_Programs.cmd
@@ -1,3 +1,9 @@
 @(REM coding:CP866
-CALL FindAutoHotkeyExe.cmd "%LOCALAPPDATA%\Scripts\nprivRun.ahk" "%~dp0install.cmd" --vivaldi-install-dir="%LOCALAPPDATA%\Programs\Vivaldi"
+SET runPrefix=
+SET runSuffix=
+%SystemRoot%\System32\fltmc.exe >nul 2>&1 && (
+    SET "runPrefix=CALL FindAutoHotkeyExe.cmd "%LOCALAPPDATA%\Scripts\nprivRun.ahk" %comspec% /C ""
+    SET "runSuffix=""
 )
+)
+%runPrefix%"%~dp0install.cmd" --vivaldi-install-dir="%LOCALAPPDATA%\Programs\Vivaldi"%runSuffix%
