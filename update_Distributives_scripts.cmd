70210e039133b9e28671b27917c7cad7253b05fd
LogicDaemon <LogicDaemon@users.noreply.github.com>
2025-07-18 20:49:41 +0400

    misc

--- update_Distributives_scripts.cmd
+++ update_Distributives_scripts.cmd
@@ -2,7 +2,7 @@
 robocopy %* ^
 	*.cmd *.ahk *.list *.py descript.ion "partial list of free SysUtils.txt" ^
 	jre_install_common.cfg opabackup*.exe.config ^
-	/S /XD .mypy_cache .venv config Drivers_local LLMs Local_Scripts ^
+	/S /XD .mypy_cache .venv config Drivers_local ota-artifacts LLMs Local_Scripts ^
 	Soft_local "Soft malicious" ^
 	wsusoffline Images
 )
