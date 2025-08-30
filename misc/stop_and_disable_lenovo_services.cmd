994317c1c28b4986791bb89172b8c6620c53d303
LogicDaemon <LogicDaemon@users.noreply.github.com>
2025-08-30 10:13:01 +0400

    misc

---
+++ misc/stop_and_disable_lenovo_services.cmd
@@ -0,0 +1,12 @@
+@(REM coding:CP866
+REM 0BSD (https://opensource.org/license/0bsd) / public domain by LogicDaemon <https://www.logicdaemon.ru/>
+SETLOCAL ENABLEEXTENSIONS
+)
+
+sc config UDCService start= demand
+sc stop UDCService
+sc config LenovoVantageService start= demand
+sc stop LenovoVantageService
+rem Lenovo Whisper Mode Service
+sc config LenovoWMService start= demand
+sc stop LenovoWMService
